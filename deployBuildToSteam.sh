a64a4502db4442f5525497b0bd396d0171705f76
Mio Engel <mioengel@gmail.com>
2017-12-14 00:18:21 +0100

    updated steam path

--- deployBuildToSteam.sh
+++ deployBuildToSteam.sh
@@ -8,4 +8,4 @@ BUILDER_DIR="/home/valhalla/sdk/tools/ContentBuilder"
 
 rsync -ax --delete "jenkins@valhalla-game.com:/home/valhalla/builds/$BUILD_VERSION/WindowsClient/WindowsNoEditor" "$BUILDER_DIR/content/windows_content"
 
-/home/valhalla/steamworks_sdk/sdk/tools/ContentBuilder/builder_linux/steamcmd.sh +login $STEAM_USER $STEAM_PASSWORD +run_app_build $BUILDER_DIR/scripts/app_build_763550.vdf +quit
+/home/valhalla/sdk/tools/ContentBuilder/builder_linux/steamcmd.sh +login $STEAM_USER $STEAM_PASSWORD +run_app_build $BUILDER_DIR/scripts/app_build_763550.vdf +quit
