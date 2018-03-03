da5d6114fb168cbd3c0580a667a2645c7d0f5b01
Mikael Berglund <mikbe@kth.se>
2018-03-03 19:06:57 +0100

    Fixed deploy to steam to use ftp

--- deployBuildToSteam.sh
+++ deployBuildToSteam.sh
@@ -2,12 +2,11 @@
 
 BUILD_VERSION=$1
 STEAM_USER=$2
-STEAM_PASSWORD=$3
-FTP_PASSWORD=$4
+FTP_PASSWORD=$3
 
 BUILDER_DIR="/home/valhalla/sdk/tools/ContentBuilder"
 rm -r $BUILDER_DIR/content/windows_content
-wget -m -nH --cut-dirs=2 -P $BUILDER_DIR/content/windows_content ftp://jenkins:$4@ftp.valhalla-game.com/$1/WindowsClient/WindowsNoEditor
+wget -m -nH --cut-dirs=2 -P $BUILDER_DIR/content/windows_content ftp://jenkins:$FTP_PASSWORD@ftp.valhalla-game.com/$BUILD_VERSION/WindowsClient/WindowsNoEditor
 #rsync -ax --delete "jenkins@valhalla-game.com:/home/valhalla/builds/$BUILD_VERSION/WindowsClient/WindowsNoEditor/" "$BUILDER_DIR/content/windows_content"
 
-/home/valhalla/sdk/tools/ContentBuilder/builder_linux/steamcmd.sh +login $STEAM_USER $STEAM_PASSWORD +run_app_build $BUILDER_DIR/scripts/app_build_763550.vdf +quit
+/home/valhalla/sdk/tools/ContentBuilder/builder_linux/steamcmd.sh +login $STEAM_USER +run_app_build $BUILDER_DIR/scripts/app_build_763550.vdf +quit
