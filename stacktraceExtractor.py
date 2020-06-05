12137071261f464ba05020415532d1da1f5807ed
Mikael Berglund <mail@mikael-berglund.se>
2020-06-05 02:21:44 +0200

    fixed stacktrace extractor

--- stacktraceExtractor.py
+++ stacktraceExtractor.py
@@ -18,34 +18,39 @@ head, crashFolderZip = os.path.split(metadata.get("pathOnDisc"))
 gameBinariesFolder = f'WindowsNoEditor{metadata.get("crashInVersion")}'
 gameBinariesZip = f'{gameBinariesFolder}.zip'
 print(f"got metadata {metadata}")
+
 print(f"Downloading crash")
-with requests.get(
-        f'https://qa.valhalla-game.com/api/crash/{crashId}/download',
-        allow_redirects=True,
-        stream=True,
-        auth=HTTPBasicAuth(user, os.environ['JENKINS_API_TOKEN'])) as r:
-    with open(crashFolderZip, 'wb') as f:
-        for chunk in r:
-            f.write(chunk)
+# with requests.get(
+#         f'https://qa.valhalla-game.com/api/crash/{crashId}/download',
+#         allow_redirects=True,
+#         stream=True,
+#         auth=HTTPBasicAuth(user, os.environ['JENKINS_API_TOKEN'])) as r:
+#     with open(crashFolderZip, 'wb') as f:
+#         for chunk in r:
+#             f.write(chunk)
+
 print(f"Downloading game binaries")
 gameUrl = f'http://gungnir.teamsamst.com:8899/storage?path=valhalla-windows-client&name={gameBinariesZip}'
-with requests.get(gameUrl, allow_redirects=True, stream=True, headers={'Authorization': binaryStorageSecret}) as r:
-    with open(gameBinariesZip, 'wb') as f:
-        for chunk in r:
-            f.write(chunk)
-print(f"Extracting game binaries")
-with zipfile.ZipFile(gameBinariesZip, 'r') as zip_ref:
-    zip_ref.extractall(gameBinariesFolder)
+# with requests.get(gameUrl, allow_redirects=True, stream=True, headers={'Authorization': binaryStorageSecret}) as r:
+#     with open(gameBinariesZip, 'wb') as f:
+#         for chunk in r:
+#             f.write(chunk)
+
+# print(f"Extracting game binaries")
+# with zipfile.ZipFile(gameBinariesZip, 'r') as zip_ref:
+#     zip_ref.extractall(gameBinariesFolder)
+
 print(f"Extracting crash")
 crashFolder = f"{os.path.abspath(gameBinariesFolder)}\\valhalla\\Saved\\Crashes\\{Path(crashFolderZip).stem}"
-with zipfile.ZipFile(crashFolderZip, 'r') as zip_ref:
-    zip_ref.extractall(crashFolder)
+# with zipfile.ZipFile(crashFolderZip, 'r') as zip_ref:
+#     zip_ref.extractall(crashFolder)
+
 print(f"Getting diagostics tool")
-with requests.get("https://github.com/saiaku-gaming/jenkins-common/raw/master/MinidumpDiagnostics.exe",
-                  stream=True) as r:
-    with open("MinidumpDiagnostics.exe", 'wb') as f:
-        for chunk in r:
-            f.write(chunk)
+# with requests.get("https://github.com/saiaku-gaming/jenkins-common/raw/master/MinidumpDiagnostics.exe",
+#                   stream=True) as r:
+#     with open("MinidumpDiagnostics.exe", 'wb') as f:
+#         for chunk in r:
+#             f.write(chunk)
 
 symbolFolder = f"{os.path.abspath(gameBinariesFolder)}\\valhalla\\Binaries\\Win64"
 print(f"Symbol folder {symbolFolder}")
@@ -56,10 +61,12 @@ popen = subprocess.Popen(args, stdout=sys.stdout, stderr=sys.stderr, env={
 code = popen.wait()
 print(f"diag code {code}")
 
-files = {'file': open(f"{crashFolder}\\Diagnostics.txt")}
-crashId = metadata.get('id')
-response = requests.post(f'https://qa.valhalla-game.com/api/crash/{crashId}/diagnostics',
-                         files=files,
-                         allow_redirects=True,
-                         auth=HTTPBasicAuth(user, os.environ['JENKINS_API_TOKEN']))
-print(f"uploaded diag with response {response}")
+with open(f"{crashFolder}\\Diagnostics.txt") as myfile:
+	data = myfile.read()
+	crashId = metadata.get('id')
+	response = requests.post(f'https://qa.valhalla-game.com/api/crash/{crashId}/diagnostics',
+							 headers={'Content-Type': 'text/plain'},
+	                         data=data,
+	                         allow_redirects=True,
+	                         auth=HTTPBasicAuth(user, os.environ['JENKINS_API_TOKEN']))
+	print(f"uploaded diag with response {response}")
