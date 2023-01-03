a153781df0af15491693555ab176b3d89f6d1e28
Mio Engel <mioengel@gmail.com>
2023-01-03 21:57:43 +0100

    Makes deploy script general and adds cyberhunt app and depot files

--- deployBuildToSteam.sh
+++ deployBuildToSteam.sh
@@ -8,6 +8,8 @@ STEAM_USER=$2
 STEAM_PASSWORD=$3
 STORAGE_SERVER_SECRET=$4
 RELEASE_VERSION=$5
+APP_ID=$6
+DEPOT_ID=$7
 if [ "$6" = "local" ]; then
       USE_LOCAL=true
 else
@@ -37,27 +39,27 @@ fi
 
 unzip WindowsNoEditor$BUILD_VERSION$RELEASE_VERSION.zip -d $BUILDER_DIR/content/windows_content
 
-APP_BUILD_NAME="dev-app_build_763550.vdf"
+APP_BUILD_NAME="dev-app_build_$APP_ID.vdf"
 
 if [ "$RELEASE_VERSION" = "Development" ]; then
-	APP_BUILD_NAME="dev-app_build_763550.vdf"
+	APP_BUILD_NAME="dev-app_build_$APP_ID.vdf"
 elif [ "$RELEASE_VERSION" = "DebugGame" ]; then
-	APP_BUILD_NAME="dev-app_build_763550.vdf"
+	APP_BUILD_NAME="dev-app_build_$APP_ID.vdf"
 elif [ "$RELEASE_VERSION" = "Shipping" ]; then
-	APP_BUILD_NAME="prod-app_build_763550.vdf"
+	APP_BUILD_NAME="prod-app_build_$APP_ID.vdf"
 fi
 
-curl https://raw.githubusercontent.com/saiaku-gaming/jenkins-common/master/$APP_BUILD_NAME > $BUILDER_DIR/scripts/app_build_763550.vdf
-curl https://raw.githubusercontent.com/saiaku-gaming/jenkins-common/master/depot_build_763551.vdf > $BUILDER_DIR/scripts/depot_build_763551.vdf
+curl https://raw.githubusercontent.com/saiaku-gaming/jenkins-common/master/$APP_BUILD_NAME > $BUILDER_DIR/scripts/app_build_$APP_ID.vdf
+curl https://raw.githubusercontent.com/saiaku-gaming/jenkins-common/master/depot_build_$DEPOT_ID.vdf > $BUILDER_DIR/scripts/depot_build_$DEPOT_ID.vdf
 
-sed -i "s/\$BUILD_VERSION/$BUILD_VERSION/g" $BUILDER_DIR/scripts/app_build_763550.vdf
+sed -i "s/\$BUILD_VERSION/$BUILD_VERSION/g" $BUILDER_DIR/scripts/app_build_$APP_ID.vdf
 
 if [ "$USE_LOCAL" = "true" ]; then
-	sed -i 's,"setlive" "development","setlive" "local",g' $BUILDER_DIR/scripts/app_build_763550.vdf
-	sed -i 's,"local" "","local" "/opt/valhalla-steam-content",g' $BUILDER_DIR/scripts/app_build_763550.vdf
+	sed -i 's,"setlive" "development","setlive" "local",g' $BUILDER_DIR/scripts/app_build_$APP_ID.vdf
+	sed -i 's,"local" "","local" "/opt/valhalla-steam-content",g' $BUILDER_DIR/scripts/app_build_$APP_ID.vdf
 fi
 
 #If below does not work, try installing support for 32-bit os.
 #sudo apt-get install libc6:i386 libncurses5:i386 libstdc++6:i386
 
-/bin/bash $BUILDER_DIR/builder_linux/steamcmd.sh +login $STEAM_USER $STEAM_PASSWORD +run_app_build $(pwd)/$BUILDER_DIR/scripts/app_build_763550.vdf +quit
+/bin/bash $BUILDER_DIR/builder_linux/steamcmd.sh +login $STEAM_USER $STEAM_PASSWORD +run_app_build $(pwd)/$BUILDER_DIR/scripts/app_build_$APP_ID.vdf +quit
