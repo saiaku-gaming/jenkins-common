ef7548c4b66d3a93db9623f5970aadeaf726d3f6
Mio Engel <mioengel@gmail.com>
2023-01-04 23:25:26 +0100

    Makes the script more general

--- deployBuildToSteam.sh
+++ deployBuildToSteam.sh
@@ -8,9 +8,10 @@ STEAM_USER=$2
 STEAM_PASSWORD=$3
 STORAGE_SERVER_SECRET=$4
 RELEASE_VERSION=$5
-APP_ID=$6
-DEPOT_ID=$7
-if [ "$6" = "local" ]; then
+CLIENT_NAME=$6
+APP_ID=$7
+DEPOT_ID=$8
+if [ "$9" = "local" ]; then
       USE_LOCAL=true
 else
       USE_LOCAL=false
@@ -18,12 +19,12 @@ fi
 
 BUILDER_DIR=SteamContentBuilder
 
-rm -rf WindowsNoEditor*
+rm -rf Windows*
 
 if [ "$USE_LOCAL" = "true" ]; then
 	echo "NOTE: Skipping SteamContentBuilder refresh for maximum speed!"
 	rm -rf $BUILDER_DIR/content/windows_content
-	cp "/opt/binary-storage/valhalla-windows-client/WindowsNoEditor$BUILD_VERSION$RELEASE_VERSION.zip" .
+	cp "/opt/binary-storage/$CLIENT_NAME/Windows$BUILD_VERSION$RELEASE_VERSION.zip" .
 else
 	rm -r $BUILDER_DIR || true
 	rm SteamContentBuilder.zip || true
@@ -34,10 +35,10 @@ else
 	chmod +x $BUILDER_DIR/builder_linux/linux32/steamcmd
 	chmod +x $BUILDER_DIR/builder_linux/steamcmd.sh
 
-	curl -sS -H "Authorization: $STORAGE_SERVER_SECRET" "https://binary-storage.valhalla-game.com/storage?path=valhalla-windows-client&name=WindowsNoEditor$BUILD_VERSION$RELEASE_VERSION.zip" --output "WindowsNoEditor$BUILD_VERSION$RELEASE_VERSION.zip"
+	curl -sS -H "Authorization: $STORAGE_SERVER_SECRET" "https://binary-storage.valhalla-game.com/storage?path=$CLIENT_NAME&name=Windows$BUILD_VERSION$RELEASE_VERSION.zip" --output "Windows$BUILD_VERSION$RELEASE_VERSION.zip"
 fi
 
-unzip WindowsNoEditor$BUILD_VERSION$RELEASE_VERSION.zip -d $BUILDER_DIR/content/windows_content
+unzip Windows$BUILD_VERSION$RELEASE_VERSION.zip -d $BUILDER_DIR/content/windows_content
 
 APP_BUILD_NAME="dev-app_build_$APP_ID.vdf"
 DEPO_BUILD_NAME="dev-depot_build_$DEPOT_ID.vdf"
@@ -60,7 +61,7 @@ sed -i "s/\$BUILD_VERSION/$BUILD_VERSION/g" $BUILDER_DIR/scripts/app_build_$APP_
 
 if [ "$USE_LOCAL" = "true" ]; then
 	sed -i 's,"setlive" "development","setlive" "local",g' $BUILDER_DIR/scripts/app_build_$APP_ID.vdf
-	sed -i 's,"local" "","local" "/opt/valhalla-steam-content",g' $BUILDER_DIR/scripts/app_build_$APP_ID.vdf
+	sed -i 's,"local" "","local" "/opt/steam-content",g' $BUILDER_DIR/scripts/app_build_$APP_ID.vdf
 fi
 
 #If below does not work, try installing support for 32-bit os.
